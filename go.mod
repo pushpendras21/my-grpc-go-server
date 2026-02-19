c75bc590ffa1035944dbd5818a2d3a833b4f338b
Pushpendra Singh <pushpendras21@gmail.com>
2026-02-19 14:24:18 +0530

    my0grpc-go-server init

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/pushpendras21/my-grpc-go-server
+
+go 1.25.4
+
+require (
+	github.com/pushpendras21/protobuf-grpc v0.0.1
+	google.golang.org/grpc v1.79.1
+)
+
+require (
+	golang.org/x/net v0.49.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
+)
---
+++ internal/adapter/grpc/server.go
@@ -0,0 +1,48 @@
+package grpc
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/pushpendras21/my-grpc-go-server/internal/port"
+	"github.com/pushpendras21/protobuf-grpc/protogen/go/hello"
+	"google.golang.org/grpc"
+)
+
+type GrpcAdapter struct {
+	helloService port.HelloServicePort
+	grpcPort     int
+	server       *grpc.Server
+	hello.HelloServiceServer
+}
+
+func NewGrpcAdapter(helloService port.HelloServicePort, grpcPort int) *GrpcAdapter {
+	return &GrpcAdapter{
+		helloService: helloService,
+		grpcPort:     grpcPort,
+	}
+
+}
+
+func (a *GrpcAdapter) Run() {
+	var err error
+
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", a.grpcPort))
+	if err != nil {
+		log.Fatalf("Failed to listen on port: %d: %v\n", a.grpcPort, err)
+	}
+	log.Printf("Server listening on port: %d\n", a.grpcPort)
+
+	grpcServer := grpc.NewServer()
+	a.server = grpcServer
+	hello.RegisterHelloServiceServer(grpcServer, a)
+
+	if err = grpcServer.Serve(listen); err != nil {
+		log.Fatalf("Failed to gRPC on port: %d: %v\n", a.grpcPort, err)
+	}
+}
+
+func (a *GrpcAdapter) Stop() {
+	a.server.Stop()
+}
